be44966c68b1a8b192da8d35ddbdd47fdd6940a9
Neilcuri7 <u20231b866@upc.edu.pe>
2026-04-18 11:09:34 -0500

    primer commit

---
+++ src/main/java/com/api/rurieats/RuriEatsApplication.java
@@ -0,0 +1,13 @@
+package com.api.rurieats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuriEatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuriEatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/rurieats/RuriEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rurieats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuriEatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
